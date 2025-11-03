5d9d8fe6aee6205df525eaf5201c430d53e058f0
Vinayachandran1709 <imvinayachandran@gmail.com>
2025-11-03 17:06:12 +0530

    worked on dictionaries and functions in py

---
+++ python/functions.py
@@ -0,0 +1,7 @@
+def happy_birthday(name, age):
+    print(f"Happy Birthday {name}")
+    print(f"You are {age} years old")
+
+happy_birthday("Tom", 21)
+happy_birthday("Mark", 30)
+happy_birthday("Ben", 40)
