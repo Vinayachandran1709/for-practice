33cbe1cbe9f55d8b6f265996a6ad5938e202a123
Vinayachandran1709 <imvinayachandran@gmail.com>
2025-11-06 21:11:05 +0530

    worked on args and kwargs

--- python/keyword arg.py
+++ python/keyword arg.py
@@ -1,3 +1,11 @@
-def wishes(greetings, first, last):
-    print(f"{greetings} {first} {last}")
-wishes("happy birthday", "vinaya", "chandran" )
+#def wishes(greetings, first, last):
+ #   print(f"{greetings} {first} {last}")
+#wishes("happy birthday", "vinaya", "chandran" )
+
+#for x in range(1, 11):
+ # print(x, end = "-")
+
+def get_code(country, code, first, last):
+  return f"{country} : {code} - {first} {last}"
+
+print(get_code(country="India",code="91", first="Vinaya", last="Chandran"))
